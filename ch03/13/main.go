0500871e2d6a37261d76004f88c0ded518f99d78
박종석 <bruce@bagjongseogs-Mac-mini.local>
2021-01-06 11:09:39 +0900

    ch03

---
+++ ch03/13/main.go
@@ -0,0 +1,27 @@
+package main
+
+const (
+	_ = 1 << (10 * iota)
+	KiB
+	MiB
+	GiB
+	TiB
+	PiB
+	EiB
+	ZiB
+	YiB
+)
+
+const (
+	KB = 1000
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
+)
+
+func main() {
+}
